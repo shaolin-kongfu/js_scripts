2766e05b032dbd2c680184756cb385b247521cb5
shaolin-kongfu <61420528+shaolin-kongfu@users.noreply.github.com>
2021-08-07 17:43:29 +0800

    Add files via upload

---
+++ jckd.boxjs.js
@@ -0,0 +1,63 @@
+{
+    "id": "xxxx.app.sub",
+    "name": "晶彩看点boxjs订阅",
+    "author": "@xxxxxx",
+    "icon": "https://raw.githubusercontent.com/xxxx/xxxx/main/xxxx/xxxx.png",
+    "repo": "https://github.com/xxxxx/xxxx",
+    "apps": [
+        {
+            "id": "jcKkz",
+            "name": "晶彩看看赚",
+            "keys": [
+              "lookStartbody", 
+              "lookbody"
+            ], 
+            "repo": "https://github.com/shaolin-kongfu/js_scripts/blob/main/jckkz.js",  
+            "script": "https://raw.githubusercontent.com/shaolin-kongfu/js_scripts/main/jckkz.js", 
+             
+            "icons": [ 
+              "https://cdn.jsdelivr.net/gh/NANXIAO1971/ceshi@main/picture/jckd1.png",
+              "https://cdn.jsdelivr.net/gh/NANXIAO1971/ceshi@main/picture/jckd1.png"  
+            ]
+          },
+          {
+            "id": "jcRead",
+            "name": "晶彩自动阅读",
+            "keys": [
+              "wzbody"
+            ], 
+            "repo": "https://github.com/shaolin-kongfu/js_scripts/blob/main/jcwz.js", 
+            "script": "https://raw.githubusercontent.com/shaolin-kongfu/js_scripts/main/jcwz.js", 
+            "icons": [ 
+              "https://cdn.jsdelivr.net/gh/NANXIAO1971/ceshi@main/picture/jckd1.png",
+              "https://cdn.jsdelivr.net/gh/NANXIAO1971/ceshi@main/picture/jckd1.png"
+            ]
+          },
+          {
+            "id": "jcSign",
+            "name": "晶彩签到",
+            "keys": [
+              "qdbody"
+            ], 
+            "repo": "https://github.com/shaolin-kongfu/js_scripts/blob/main/jcqd.js",
+            "script": "https://raw.githubusercontent.com/shaolin-kongfu/js_scripts/main/jcqd.js",     
+            "icons": [ 
+              "https://cdn.jsdelivr.net/gh/NANXIAO1971/ceshi@main/picture/jckd1.png",
+              "https://cdn.jsdelivr.net/gh/NANXIAO1971/ceshi@main/picture/jckd1.png"
+            ]
+          },
+          {
+            "id": "jcZf",
+            "name": "晶彩火爆转发",
+            "keys": [
+              "jc_cookie"
+            ], 
+            "repo": "https://github.com/shaolin-kongfu/js_scripts/blob/main/jc_share.js",
+            "script": "https://raw.githubusercontent.com/shaolin-kongfu/js_scripts/main/jc_share.js",     
+            "icons": [ 
+              "https://cdn.jsdelivr.net/gh/NANXIAO1971/ceshi@main/picture/jckd1.png",
+              "https://cdn.jsdelivr.net/gh/NANXIAO1971/ceshi@main/picture/jckd1.png"
+            ]
+          }          
+    ]
+}
