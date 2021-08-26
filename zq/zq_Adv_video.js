1f06bf54788cc21e2d284598b994960777caa088
shaolin-kongfu <61420528+shaolin-kongfu@users.noreply.github.com>
2021-08-26 19:10:11 +0800

    Update zq_Adv_video.js

--- zq/zq_Adv_video.js
+++ zq/zq_Adv_video.js
@@ -32,7 +32,7 @@ if (zq_cookie) {
         zq_cookieArr = process.env.zq_cookie.split('@');
         console.log(`您选择的是用"@"隔开\n`)
     }
-} else {
+} else if($.isNode()){
     var fs = require("fs");
     zq_cookie = fs.readFileSync("zq_cookie.txt", "utf8");
     if (zq_cookie !== `undefined`) {
