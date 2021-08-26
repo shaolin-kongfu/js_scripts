277063210f146bbb2fb9bdaf76f781b34c13f082
shaolin-kongfu <61420528+shaolin-kongfu@users.noreply.github.com>
2021-08-26 19:06:39 +0800

    Update zq_withdraw.js

--- zq/zq_withdraw.js
+++ zq/zq_withdraw.js
@@ -50,7 +50,7 @@ if (zq_withdraw) {
         zq_withdrawArr = process.env.zq_withdraw.split('@');
         console.log(`您选择的是用"@"隔开\n`)
     }
-} else {
+} else if($.isNode()){
     var fs = require("fs");
     zq_withdraw = fs.readFileSync("zq_withdraw.txt", "utf8");
     if (zq_withdraw !== `undefined`) {
